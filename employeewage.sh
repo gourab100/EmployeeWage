cadd87bd751c9bec5350548d948a471c3ddaf55d
gourab <gourab.saheb100@gmail.com>
2022-06-03 14:52:06 +0530

    Refactor:UC3: employee parttime daliywage is done

--- employeewage.sh
+++ employeewage.sh
@@ -1,13 +1,22 @@
 echo " welcome to  employeewage"
 WAGE_PER_HOUR=20
 FULL_DAY_HOURS=8
+HALF_DAY_HOURS=4
 absent=0
+partTime=2
+fullTime=1
 
 attendance=$((RANDOM%3))
 if [[ $attendance -eq $absent ]]
 then
 	echo "Employee is absent"
 	dailyWage=0
+elif [[ $attendance -eq $partTime ]]
+then
+	echo "Employee is parttime"
+	dailyWage=$(($WAGE_PER_HOUR * $HALF_DAY_HOURS))
+        echo "Daily wage for part time: $dailyWage"
+
 else
 	echo "Employee is present"
 	dailyWage=$(($WAGE_PER_HOUR * $FULL_DAY_HOURS))
