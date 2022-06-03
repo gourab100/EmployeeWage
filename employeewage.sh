55b57776e006ec4bb91f26d9b2cfd9a0138d798c
gourab <gourab.saheb100@gmail.com>
2022-06-03 14:44:53 +0530

    Refactor:UC2:calculate daliy wage

--- employeewage.sh
+++ employeewage.sh
@@ -1,9 +1,15 @@
 echo " welcome to  employeewage"
+WAGE_PER_HOUR=20
+FULL_DAY_HOURS=8
 absent=0
-attendance=$((RANDOM%2))
+
+attendance=$((RANDOM%3))
 if [[ $attendance -eq $absent ]]
 then
 	echo "Employee is absent"
+	dailyWage=0
 else
 	echo "Employee is present"
+	dailyWage=$(($WAGE_PER_HOUR * $FULL_DAY_HOURS))
+	echo "Daily wage for full time: $dailyWage"
 fi
